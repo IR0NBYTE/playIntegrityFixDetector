bb84cee7a94fc372f60018bc49c05408d7588428
ir0nbyte <ouachani.tuto@gmail.com>
2026-06-30 10:24:24 +0100

    bump version to 2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.ir0nbyte.pifdetector"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "2.3"
+        versionCode = 6
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
